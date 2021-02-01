482eba205d6083a5d9e5c8be65f9428f9c312b74
Bhupendra <bhupendra1011@gmail.com>
2021-02-01 22:50:48 +0530

    authentication flow complete ✔

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { SafeAreaView } from "react-navigation";
 import { Button } from "react-native-elements";
 import { StyleSheet, Text, View } from "react-native";
 import Spacer from "../components/Spacer";
@@ -7,12 +8,12 @@ import { Context as AuthContext } from "../context/AuthContext";
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
   return (
-    <>
+    <SafeAreaView forceInset={{ top: "always" }}>
       <Text> AccountScreen</Text>
       <Spacer>
         <Button title="Sign Out" onPress={signout} />
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 
