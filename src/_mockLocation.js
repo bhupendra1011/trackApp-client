702a92ced9e1d41875dcad5fabdc402cc678d6ba
Bhupendra <bhupendra1011@gmail.com>
2021-02-08 07:29:52 +0530

    setting up user location

---
+++ src/_mockLocation.js
@@ -0,0 +1,25 @@
+import * as Location from "expo-location";
+
+const variationInDegree = 0.0001;
+
+const getLocation = (increment) => {
+  return {
+    timestamp: 10000000,
+    coords: {
+      speed: 0,
+      heading: 0,
+      accuracy: 5,
+      altitudeAccuracy: 5,
+      altitude: 5,
+      latitude: 30.3165 + increment * variationInDegree,
+      longitude: 78.032 + increment * variationInDegree,
+    },
+  };
+};
+
+setInterval(() => {
+  Location.EventEmitter.emit("Expo.locationChanged", {
+    watchId: Location._getCurrentWatchId(),
+    location: getLocation(Math.floor(Math.random() * 1000)),
+  });
+}, 1000);
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-navigation";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
+import {
+  requestPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
 import Map from "../components/Map";
+import "../_mockLocation";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => console.log(location)
+      );
+      setErr(null);
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
-      <Text h2>Create a Track 1</Text>
+      <Text h2>Create a Track </Text>
       <Map />
+      {err ? <Text>Please Enable location services</Text> : null}
     </SafeAreaView>
   );
 };
